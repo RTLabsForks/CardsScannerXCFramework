e79a8b061f759332f07ccb8a36352aed3973fdbd
Sergey Safiullin <sergey.safiullin@rtlabs.ru>
2021-04-13 16:04:21 +0300

    upload xcframework

---
+++ CardsScanner.xcframework/ios-arm64/CardsScanner.framework/Headers/CardsScanner.h
@@ -0,0 +1,20 @@
+//
+//  CardsScanner.h
+//  CardsScanner
+//
+//  Created by Vadim Shilov on 21/11/2019.
+//  Copyright © 2019 RTLabs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "CardIO.h"
+
+//! Project version number for CardsScanner.
+FOUNDATION_EXPORT double CardsScannerVersionNumber;
+
+//! Project version string for CardsScanner.
+FOUNDATION_EXPORT const unsigned char CardsScannerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CardsScanner/PublicHeader.h>
+
+
